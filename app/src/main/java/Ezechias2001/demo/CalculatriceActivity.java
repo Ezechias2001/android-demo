9d2e3e0fd5bf94265ce5bfa8b9439ef91b8dd320
Ezechias <ezechiasgdv@gmail.com>
2021-11-25 11:32:06 +0100

    creation de l'interface de la calculatrice (#5)

---
+++ app/src/main/java/Ezechias2001/demo/CalculatriceActivity.java
@@ -0,0 +1,14 @@
+package Ezechias2001.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatriceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+    }
+}
